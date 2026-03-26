b13175aa7986fc3b3e5183be3e258fc6b11bf343
Grant Chen <grantchen2003@gmail.com>
2026-03-26 16:31:00 -0400

    feat: add vpc_cidr_block output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "cdb_vpc_id" {
     value       = aws_vpc.cdb_vpc.id
 }
 
+output "cdb_vpc_cidr_block" {
+    description = "CIDR block of the shared VPC"
+    value       = aws_vpc.cdb_vpc.cidr_block
+}
+
 output "cdb_private_subnet_id" {
     description = "ID of the private subnet"
     value       = aws_subnet.cdb_private_subnet.id
